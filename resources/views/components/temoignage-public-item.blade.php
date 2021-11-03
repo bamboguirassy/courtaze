7ec3f5c4fd3035b035264ae1565e5a83a572f76f
abdouazizsy <ziza97tiv@gmail.com>
2021-11-03 12:54:06 +0000

    Implementation TemoignagePubliItem final

--- resources/views/components/temoignage-public-item.blade.php
+++ resources/views/components/temoignage-public-item.blade.php
@@ -1,19 +1,26 @@
 <div>
-    <p class="mbr-text mbr-fonts-style mb-4 display-7">
-        <em>{{$temoignage->message}}</em>
-    </p>
-    <div class="d-flex mb-md-0 mb-4">
-        <div class="image-wrapper">
+   @foreach ($temoignage as $temoignages )
+   <p class="mbr-text mbr-fonts-style mb-4 display-7">
+    <em>{{$temoignages->message}}</em>
+</p>
+<div class="d-flex mb-md-0 mb-4">
+    <div class="image-wrapper">
+        @if ($temoignages->photo)
             <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt=""
-                loading="lazy" class="lazyload" data-src="{{ asset('uploads/temoignage/client/'.$temoignage->photo) }}">
-        </div>
-        <div class="text-wrapper my-auto">
-            <p class="name mbr-fonts-style mb-1 display-5">
-                <strong>{{$temoignage->fonction}}</strong>
-            </p>
-            <p class="position mbr-fonts-style display-4">
-                <strong>{{$temoignage->clientName}}</strong>
-            </p>
-        </div>
+            loading="lazy" class="lazyload" data-src="{{ asset('uploads/temoignage/client/'.$temoignages->photo) }}">
+        @else
+        <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="" loading="lazy" class="w-100 lazyload" data-src="assets/images/images.png">
+        @endif
     </div>
+    <div class="text-wrapper my-auto">
+        <p class="name mbr-fonts-style mb-1 display-5">
+            <strong>{{$temoignages->fonction}}</strong>
+        </p>
+        <p class="position mbr-fonts-style display-4">
+            <strong>{{$temoignages->clientName}}</strong>
+        </p>
+    </div>
+</div>
+   @endforeach
+
 </div>
